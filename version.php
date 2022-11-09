0d1c8a32f4faac3db407da48fa891252d765f67b
Kevin Pham <keevan.pham@gmail.com>
2022-11-09 16:07:30 +1100

    feat: add boilerplate files

---
+++ version.php
@@ -0,0 +1,36 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Version
+ *
+ * @package   tool_fileredact
+ * @author    Kevin Pham <kevinpham@catalyst-au.net>
+ * @copyright Catalyst IT, 2022
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version = 2022110900;
+$plugin->release = 2022110900;
+$plugin->requires = 2017051500;    // Moodle 3.3 for Totara support.
+$plugin->supported = [35, 401];     // Supports Moodle 3.5 or later.
+
+$plugin->component = 'tool_fileredact';
+$plugin->maturity  = MATURITY_ALPHA;
+
+$plugin->dependencies = [];
